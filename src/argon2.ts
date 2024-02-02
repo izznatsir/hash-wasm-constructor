620fc610d3299de33125965badc82ea87715cbf4
Izzuddin Natsir <32354148+izznatsir@users.noreply.github.com>
2024-02-02 15:14:20 +0700

    feat: hasher blueprint

--- src/argon2.ts
+++ src/argon2.ts
@@ -1,3 +1,5 @@
+import { Hasher } from "./hasher.js";
+
 interface Argon2ConstructorArgs {
 	wasm: {
 		argon2: WebAssembly.Instance | WebAssembly.Module;
@@ -6,21 +8,12 @@ interface Argon2ConstructorArgs {
 }
 
 class Argon2 {
-	#argon2_wasm: PromiseLike<WebAssembly.Instance>;
-	#blak2b_wasm: PromiseLike<WebAssembly.Instance>;
+	#argon2_hasher: Hasher;
+	#blak2b_hasher: Hasher;
 
 	constructor({ wasm }: Argon2ConstructorArgs) {
-		if (wasm.argon2 instanceof WebAssembly.Instance) {
-			this.#argon2_wasm = Promise.resolve(wasm.argon2);
-		} else {
-			this.#argon2_wasm = WebAssembly.instantiate(wasm.argon2);
-		}
-
-		if (wasm.blak2b instanceof WebAssembly.Instance) {
-			this.#blak2b_wasm = Promise.resolve(wasm.blak2b);
-		} else {
-			this.#blak2b_wasm = WebAssembly.instantiate(wasm.blak2b);
-		}
+		this.#argon2_hasher = new Hasher({ wasm: wasm.argon2 });
+		this.#blak2b_hasher = new Hasher({ wasm: wasm.blak2b });
 	}
 }
 
--- src/hasher.ts
+++ src/hasher.ts
@@ -1,3 +1,43 @@
-class Hasher {}
+interface HasherConstructorArgs {
+	wasm: WebAssembly.Instance | WebAssembly.Module;
+}
+
+class Hasher {
+	#wasm_promise: PromiseLike<WebAssembly.Instance>;
+
+	blockSize = 0;
+
+	digestSize = 0;
+
+	hashLength = 0;
+
+	constructor({ wasm }: HasherConstructorArgs) {
+		if (wasm instanceof WebAssembly.Instance) {
+			this.#wasm_promise = Promise.resolve(wasm);
+		} else {
+			this.#wasm_promise = WebAssembly.instantiate(wasm);
+		}
+	}
+
+	calculate() {}
+
+	digest() {}
+
+	getExports() {}
+
+	getMemory() {}
+
+	init() {}
+
+	load() {}
+
+	save() {}
+
+	setMemorySize() {}
+
+	update() {}
+
+	writeMemory() {}
+}
 
 export { Hasher };
