fd734569dccdb16c72007c0e41a1400199642101
Izzuddin Natsir <32354148+izznatsir@users.noreply.github.com>
2024-02-06 10:41:15 +0700

    chore: reset argon2 implementation

--- src/argon2.ts
+++ src/argon2.ts
@@ -1,5 +1,3 @@
-import { Hasher } from "./hasher.js";
-
 interface Argon2ConstructorArgs {
 	wasm: {
 		argon2: WebAssembly.Instance | WebAssembly.Module;
@@ -7,15 +5,7 @@ interface Argon2ConstructorArgs {
 	};
 }
 
-class Argon2 {
-	#argon2_hasher: Hasher;
-	#blak2b_hasher: Hasher;
-
-	constructor({ wasm }: Argon2ConstructorArgs) {
-		this.#argon2_hasher = new Hasher({ wasm: wasm.argon2 });
-		this.#blak2b_hasher = new Hasher({ wasm: wasm.blak2b });
-	}
-}
+class Argon2 {}
 
 export { Argon2 };
 export type { Argon2ConstructorArgs };
