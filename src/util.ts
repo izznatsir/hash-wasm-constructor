9541f8f8c3bd5ec8d08af43969fad2122c5c1c91
Izzuddin Natsir <32354148+izznatsir@users.noreply.github.com>
2024-02-06 10:39:48 +0700

    chore: write some utils

--- src/util.ts
+++ src/util.ts
@@ -2,6 +2,101 @@ import type { Data } from "./types.js";
 
 let text_encoder = new TextEncoder();
 
+let BASE64_CHARS =
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+let BASE64_LOOKUP = new Uint8Array(256);
+for (let i = 0; i < BASE64_CHARS.length; i++) {
+	BASE64_LOOKUP[BASE64_CHARS.charCodeAt(i)] = i;
+}
+
+function encodeBase64({
+	data,
+	pad = true,
+}: {
+	data: Uint8Array;
+	pad?: boolean;
+}): string {
+	let len = data.length;
+	let extra_bytes = len % 3;
+	let parts = [];
+
+	let len2 = len - extra_bytes;
+	for (let i = 0; i < len2; i += 3) {
+		let tmp =
+			((data[i] << 16) & 0xff0000) +
+			((data[i + 1] << 8) & 0xff00) +
+			(data[i + 2] & 0xff);
+
+		let triplet =
+			BASE64_CHARS.charAt((tmp >> 18) & 0x3f) +
+			BASE64_CHARS.charAt((tmp >> 12) & 0x3f) +
+			BASE64_CHARS.charAt((tmp >> 6) & 0x3f) +
+			BASE64_CHARS.charAt(tmp & 0x3f);
+
+		parts.push(triplet);
+	}
+
+	if (extra_bytes === 1) {
+		let tmp = data[len - 1];
+		let a = BASE64_CHARS.charAt(tmp >> 2);
+		let b = BASE64_CHARS.charAt((tmp << 4) & 0x3f);
+
+		parts.push(`${a}${b}`);
+		if (pad) {
+			parts.push("==");
+		}
+	} else if (extra_bytes === 2) {
+		let tmp = (data[len - 2] << 8) + data[len - 1];
+		let a = BASE64_CHARS.charAt(tmp >> 10);
+		let b = BASE64_CHARS.charAt((tmp >> 4) & 0x3f);
+		let c = BASE64_CHARS.charAt((tmp << 2) & 0x3f);
+		parts.push(`${a}${b}${c}`);
+		if (pad) {
+			parts.push("=");
+		}
+	}
+
+	return parts.join("");
+}
+
+function getDecodeBase64Length({ data }: { data: string }): number {
+	let buffer_length = Math.floor(data.length * 0.75);
+	let len = data.length;
+
+	if (data[len - 1] === "=") {
+		buffer_length -= 1;
+		if (data[len - 2] === "=") {
+			buffer_length -= 1;
+		}
+	}
+
+	return buffer_length;
+}
+
+function decodeBase64({ data }: { data: string }): Uint8Array {
+	let buffer_length = getDecodeBase64Length({ data });
+	let len = data.length;
+
+	let bytes = new Uint8Array(buffer_length);
+
+	let p = 0;
+	for (let i = 0; i < len; i += 4) {
+		let encoded1 = BASE64_LOOKUP[data.charCodeAt(i)];
+		let encoded2 = BASE64_LOOKUP[data.charCodeAt(i + 1)];
+		let encoded3 = BASE64_LOOKUP[data.charCodeAt(i + 2)];
+		let encoded4 = BASE64_LOOKUP[data.charCodeAt(i + 3)];
+
+		bytes[p] = (encoded1 << 2) | (encoded2 >> 4);
+		p += 1;
+		bytes[p] = ((encoded2 & 15) << 4) | (encoded3 >> 2);
+		p += 1;
+		bytes[p] = ((encoded3 & 3) << 6) | (encoded4 & 63);
+		p += 1;
+	}
+
+	return bytes;
+}
+
 let alpha = "a".charCodeAt(0) - 10;
 let digit = "0".charCodeAt(0);
 
@@ -16,10 +111,10 @@ function getDigestHex({
 }) {
 	let p = 0;
 
-	for (let byte of input) {
-		let nibble = byte >>> 4;
+	for (let i = 0; i < length; i++) {
+		let nibble = input[i] >>> 4;
 		chars[p++] = nibble > 9 ? nibble + alpha : nibble + digit;
-		nibble = byte & 0xf;
+		nibble = input[i] & 0xf;
 		chars[p++] = nibble > 9 ? nibble + alpha : nibble + digit;
 	}
 
@@ -86,4 +181,12 @@ function writeHexToUint8({ buffer, hex }: { buffer: Uint8Array; hex: string }) {
 	}
 }
 
-export { getDigestHex, getUint8Buffer, hexStringEqualUint8, writeHexToUint8 };
+export {
+	decodeBase64,
+	encodeBase64,
+	getDecodeBase64Length,
+	getDigestHex,
+	getUint8Buffer,
+	hexStringEqualUint8,
+	writeHexToUint8,
+};
